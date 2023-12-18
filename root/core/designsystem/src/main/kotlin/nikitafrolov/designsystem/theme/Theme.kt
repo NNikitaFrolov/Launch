42673ed6b41b87b402ba02b0a8c790879a38ccc2
Nikita <nnikita.frolov@gmail.com>
2023-12-18 18:49:40 +0200

    implement title

--- root/core/designsystem/src/main/kotlin/nikitafrolov/designsystem/theme/Theme.kt
+++ root/core/designsystem/src/main/kotlin/nikitafrolov/designsystem/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -82,9 +83,9 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun LaunchTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -100,7 +101,7 @@ fun LaunchTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
--- root/feature/exchanger/src/main/kotlin/nikitafrolov/exchanger/ui/ExchangerScreen.kt
+++ root/feature/exchanger/src/main/kotlin/nikitafrolov/exchanger/ui/ExchangerScreen.kt
@@ -1,15 +1,25 @@
 package nikitafrolov.exchanger.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import nikitafrolov.designsystem.theme.LaunchTheme
+import nikitafrolov.feature.exchanger.R
 
 const val exchangerRoute = "exchanger"
 
@@ -36,9 +46,22 @@ private fun ExchangerPreview() {
 
 @Composable
 private fun ExchangerContent() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Text(text = "Hello World!", modifier = Modifier.wrapContentSize())
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary),
+        ) {
+            Text(
+                text = stringResource(R.string.exchanger__title),
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .windowInsetsPadding(WindowInsets.statusBars)
+                    .align(Alignment.Center)
+                    .padding(16.dp, 16.dp)
+            )
+        }
+
     }
 }
