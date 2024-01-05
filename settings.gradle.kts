1302515ed249c3feb2196b74e028b522963a26b9
Nikita <nnikita.frolov@gmail.com>
2024-01-05 21:39:39 +0200

    improve build performance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,16 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version("3.16.1")
+}
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 rootProject.name = "Launch"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
